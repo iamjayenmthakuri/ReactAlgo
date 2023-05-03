e033348919b29711e42e9df8c08872f5b1d3d9d2
iamjayenmthakuri <malla.jayen@gmail.com>
2023-05-03 15:09:37 +0545

    completd and incompleted in different side

--- reactalgo/src/App.jsx
+++ reactalgo/src/App.jsx
@@ -1,33 +1,80 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
+import { Alert, Space } from "antd";
 
-import axios from "axios";
-import Todo from "./Todo";
-import { Flex, Heading } from "@chakra-ui/react";
-
-function App() {
+function TodoList() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      let results = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos/"
-      );
-      setTodos(results.data);
-    };
-    fetchTodos();
+    const timer = setTimeout(() => {
+      const fetchTodos = async () => {
+        const results = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const todos = await results.json();
+        setTodos(todos);
+      };
+      fetchTodos();
+    }, 3000);
+
+    return () => clearTimeout(timer);
   }, []);
+
+  const alertColor = (completed) => {
+    if (completed === true) {
+      return "success";
+    } else {
+      return "error";
+    }
+  };
+
+  const completedTodos = todos
+    .filter((todo) => todo.completed === true)
+    .slice(0, 10);
+  const incompleteTodos = todos
+    .filter((todo) => todo.completed === false)
+    .slice(0, 10);
 
   return (
     <>
-      <Flex justifyContent={"center"}>
-        <Heading>Algo Test</Heading>
-      </Flex>
-      {todos &&
-        todos
-          .slice(0, 10)
-          .map((todo) => <Todo key={todo.id} todo={todo} todos={todos} />)}
+      <Space
+        direction="vertical"
+        style={{
+          width: "45%",
+          margin: "10px",
+        }}
+      >
+        {completedTodos.map((todo) => (
+          <Alert
+            key={todo.id}
+            message="Completed"
+            type={alertColor(todo.completed)}
+            description={todo.title}
+            showIcon
+            closable
+          />
+        ))}
+      </Space>
+      <Space
+        direction="vertical"
+        style={{
+          width: "45%",
+          margin: "10px",
+        }}
+      >
+        {incompleteTodos.map((todo) => (
+          <Alert
+            key={todo.id}
+            message="Incomplete"
+            type={alertColor(todo.completed)}
+            description={todo.title}
+            showIcon
+            closable
+          />
+        ))}
+      </Space>
     </>
   );
 }
 
-export default App;
+export default TodoList;
--- reactalgo/src/Todo.jsx
+++ reactalgo/src/Todo.jsx
@@ -1,60 +1,73 @@
-/* eslint-disable react/prop-types */
-import { Alert, Space } from "antd";
+// import { useState } from "react";
+// import { Alert, Space } from "antd";
 
-function TodoList({ todo, todos }) {
-  const alertColor = (completed) => {
-    if (completed === true) {
-      return "success";
-    } else {
-      return "error";
-    }
-  };
+// function TodoList({ todos }) {
+//   const alertColor = (completed) => {
+//     if (completed === true) {
+//       return "success";
+//     } else {
+//       return "error";
+//     }
+//   };
 
-  const Status = todo.completed ? "Completed" : "Incomplete";
+//   const [completedCount, setCompletedCount] = useState(0);
+//   const [incompletedCount, setIncompletedCount] = useState(0);
 
-  const completedTodos = todos.filter((todo) => todo.completed === true);
-  const incompleteTodos = todos.filter((todo) => todo.completed === false);
+//   const completedTodos = todos
+//     .filter((todo) => todo.completed === true)
+//     .slice(completedCount, completedCount + 10);
+//   const incompleteTodos = todos
+//     .filter((todo) => todo.completed === false)
+//     .slice(incompletedCount, incompletedCount + 10);
 
-  return (
-    <>
-      <Space
-        direction="vertical"
-        style={{
-          width: "45%",
-          margin: "10px",
-        }}
-      >
-        {completedTodos.map((todo) => (
-          <Alert
-            key={todo.id}
-            message={Status}
-            type={alertColor(todo.completed)}
-            description={todo.title}
-            showIcon
-            closable
-          />
-        ))}
-      </Space>
-      <Space
-        direction="vertical"
-        style={{
-          width: "45%",
-          margin: "10px",
-        }}
-      >
-        {incompleteTodos.map((todo) => (
-          <Alert
-            key={todo.id}
-            message={Status}
-            type={alertColor(todo.completed)}
-            description={todo.title}
-            showIcon
-            closable
-          />
-        ))}
-      </Space>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Space
+//         direction="vertical"
+//         style={{
+//           width: "45%",
+//           margin: "10px",
+//         }}
+//       >
+//         {completedTodos.map((todo) => (
+//           <Alert
+//             key={todo.id}
+//             message="Completed"
+//             type={alertColor(todo.completed)}
+//             description={todo.title}
+//             showIcon
+//             closable
+//           />
+//         ))}
+//       </Space>
+//       <Space
+//         direction="vertical"
+//         style={{
+//           width: "45%",
+//           margin: "10px",
+//         }}
+//       >
+//         {incompleteTodos.map((todo) => (
+//           <Alert
+//             key={todo.id}
+//             message="Incomplete"
+//             type={alertColor(todo.completed)}
+//             description={todo.title}
+//             showIcon
+//             closable
+//           />
+//         ))}
+//       </Space>
+//       <div style={{ display: "flex", justifyContent: "center" }}>
+//         <button onClick={() => setCompletedCount(completedCount + 10)}>
+//           Load More Completed Todos
+//         </button>
+//         <button onClick={() => setIncompletedCount(incompletedCount + 10)}>
+//           Load More Incompleted Todos
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
 
-export default TodoList;
+// export default TodoList;
